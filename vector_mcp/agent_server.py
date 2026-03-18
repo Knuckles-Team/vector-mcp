f8d507a5472fa431a1227e56b0f656040dafff77
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-18 00:22:13 -0500

    Bump version: 1.1.48 → 1.1.49

--- vector_mcp/agent_server.py
+++ vector_mcp/agent_server.py
@@ -12,7 +12,7 @@ from agent_utilities import (
     load_identity,
 )
 
-__version__ = "1.1.48"
+__version__ = "1.1.49"
 
 logging.basicConfig(
     level=logging.INFO,
--- vector_mcp/mcp_server.py
+++ vector_mcp/mcp_server.py
@@ -21,7 +21,7 @@ from agent_utilities.mcp_utilities import (
 )
 from vector_mcp.retriever.retriever import RAGRetriever
 
-__version__ = "1.1.48"
+__version__ = "1.1.49"
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
